de76af5d6ae1601915a15ee0a36f5974b766e4d4
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-19 09:29:58 +0530

    'prime'

---
+++ Java/print.java
@@ -0,0 +1,24 @@
+public class print {
+    
+    public static void main(String[] args){
+
+        int n = 19;
+
+        for(int i=2; i<=n; i++){
+
+            int count = 0;
+            for(int p=2; p*p<i; p++){
+              
+                if(i%p==0){
+                    count++;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println(i+" is a Prmie Number");
+            }
+        }
+
+
+    }
+}
