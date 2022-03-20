bc879db1d2dedf043cbd74cfd819b47d094312c5
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-20 23:24:52 +0530

    test

---
+++ Java/subsequence.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class subsequence {
+
+    public static void main(String[] args) {
+
+        String str = "abcd";
+
+    }
+
+    public static void sub(String str, int n) {
+
+    }
+
+}
