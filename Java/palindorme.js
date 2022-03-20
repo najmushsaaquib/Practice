e3f2b6ac23df566973c6a01774137d312eeb48f1
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-21 00:16:31 +0530

    'pal'

--- Java/palindorme.js
+++ Java/palindorme.js
@@ -21,3 +21,30 @@ function generatePermutations(arr, index) {
 function swap(arr, i, j) {
   [arr[i], arr[j]] = [arr[j], arr[i]];
 }
+
+
+function runProgram(input=''){
+  
+    
+}
+
+if (process.env.USER === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
